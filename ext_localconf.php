f6c8d7e4d867ca771bf0ca5b0fe49177cacc49d7
Peter Kraume <peter.kraume@gmx.de>
2015-05-06 20:26:52 +0200

    [TASK] code optimization, part II

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,11 @@
 <?php
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
+if (ExtensionManagementUtility::isLoaded('news')) {
 	// add page TSConfig which extends the news types
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/mod.tx_news_domain_model_news.txt">');
+	ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/mod.tx_news_domain_model_news.txt">');
 }
