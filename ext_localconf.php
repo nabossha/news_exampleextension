88bcbfde616cbe70a42b9d3f310039187282b666
Peter Kraume <peter.kraume@gmx.de>
2015-12-02 08:59:27 +0100

    remove usage of 'use' to prevent merge conflicts

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,10 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (ExtensionManagementUtility::isLoaded('news')) {
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
 	// add page TSConfig which extends the news types
-	ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/mod.tx_news_domain_model_news.txt">');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/mod.tx_news_domain_model_news.txt">');
 }
